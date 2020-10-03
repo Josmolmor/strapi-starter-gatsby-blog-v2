35ec7cfcefc3a84354c0c063e1317518207e312a
Molina <molina@z1.digital>
2020-10-03 22:38:06 +0200

    chore: lint + styled components

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -1,54 +1,51 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
+require('dotenv').config({
+    path: `.env.${process.env.NODE_ENV}`,
 })
 
 module.exports = {
-  siteMetadata: {
-    title: "Strapi Gatsby Blog Starter",
-    titleTemplate: "%s · Unleash content",
-    description:
-      "Strapi Gatsby Blog Starter",
-    url: process.env.API_URL || "http://localhost:1337", // No trailing slash allowed!
-    image: "/uploads/default-image.jpeg", // Path to your image you placed in the 'static' folder
-    twitterUsername: "@you",
-  },
-  plugins: [
-    "gatsby-plugin-react-helmet",
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    siteMetadata: {
+        title: 'Virginia Otero Perez',
+        titleTemplate: '%s',
+        description: 'Terapeuta de Crecimiento y Desarrollo Personal',
+        url: process.env.API_URL || 'http://localhost:1337', // No trailing slash allowed!
+        image: '/uploads/default-image.jpeg', // Path to your image you placed in the 'static' folder
+        twitterUsername: '@virllyotero',
+        instagramUsername: '@ella.vop',
     },
-    `gatsby-plugin-mdx`,
-    {
-      resolve: "gatsby-source-strapi",
-      options: {
-        apiURL: process.env.API_URL || "http://localhost:1337",
-        contentTypes: [
-          "article",
-          "category",
-          "user"
-        ],
-        singleTypes: [`Homepage`],
-        queryLimit: 1000,
-      },
-    },
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-sharp",
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: "gatsby-starter-default",
-        short_name: "starter",
-        start_url: "/",
-        background_color: "#663399",
-        theme_color: "#663399",
-        display: "minimal-ui",
-      },
-    },
-    "gatsby-plugin-offline",
-    "gatsby-plugin-typescript"
-  ],
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        `gatsby-plugin-mdx`,
+        {
+            resolve: 'gatsby-source-strapi',
+            options: {
+                apiURL: process.env.API_URL || 'http://localhost:1337',
+                contentTypes: ['article', 'category', 'user'],
+                singleTypes: [`Homepage`],
+                queryLimit: 1000,
+            },
+        },
+        'gatsby-transformer-sharp',
+        'gatsby-plugin-sharp',
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: 'gatsby-starter-default',
+                short_name: 'starter',
+                start_url: '/',
+                background_color: '#663399',
+                theme_color: '#663399',
+                display: 'minimal-ui',
+            },
+        },
+        'gatsby-plugin-offline',
+        'gatsby-plugin-typescript',
+        'gatsby-plugin-styled-components',
+    ],
 }
